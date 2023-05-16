10873c6bbd9bcaf6e1d821a0d51cd823143a1c8f
Pål Walfridsson <pal@darktown.se>
2023-05-16 17:11:34 +0200

    Reading config from Imagine web server
    nms-config.js
    Clean up

--- mqtt-ws-proxy.js
+++ mqtt-ws-proxy.js
@@ -1,91 +1,110 @@
-
+const http = require('http');
 const mqtt = require('mqtt');
 const { Server } = require('ws');
 const config = require('./config.json');
-// const config = {
-//   "mqttIP": "192.168.1.25", 
-//   "mqttPort": "1883", 
-//   "myWebsocketPort": "8086"
-// };
 
 
 
-function isValidJSON(text) {
-  try {
-    JSON.parse(text);
-    return true;
-  } catch {
-    return false;
-  }
-}
+// Read NMS Config file
+let NMSConfig;
+http.get("http://localhost/imagine/nms-config.js", (response) => {
+  let data = '';
 
-// let test = require('../imagine/imagine_gui/app/uiStrings_en.json');
-// let test = require('C:/inetpub/wwwroot/imagine/uiStrings_en.json');
-// console.log(test);
+  response.on('data', (chunk) => {
+    data += chunk;
+  });
 
-// fetch('http://192.168.3.2/imagine/nms-config.js', {method: 'GET'})
-// .then(respone => respone.text())
-// .then(text => console.log(text));
+  response.on('end', () => {
+    data = data.split('=')[1]; // get the json content. The original data starts with "var NMSConfig = {"
+    let rowsWithComments = data.split('\n');
+    let rows = [];
+    rowsWithComments.forEach(row => {
+      row = row.trim(); // remove spaces before //
+      row = row.replace(';', ''); // remove semicolon command ends
+      if (!row.startsWith('//')) {
+        rows.push(row);
+      }
+    });
+    NMSConfig = JSON.parse(rows.join('\n'));
+    console.log('Reading data from NMSConfig file of Imagine server:', NMSConfig);
+    init();
+  });
+
+}).on('error', (err) => {
+  console.log('Error getting file:', err);
+});
+
+
+
+
 
 // WS
 // Handling Websocket connection to the browser/GUI
-const ws = new Server({port: config.myWebsocketPort});
-console.log(' ');
-console.log(' ');
-console.log(' ');
-console.log(`Websocket server listening on port ws://localhost:${config.myWebsocketPort}`);
+
 const connections = new Set();
 const messageQueue = [];
 
 
-// check the queue 
-setInterval(() => {
-  if (connections.size > 0) {
-    while (messageQueue.length > 0) {
-      let data = messageQueue.pop();
-      sendToWS(data);
-    }
-  }
-}, 1000);
+function init() {
 
+  const ws = new Server({port: NMSConfig.mqttWebsocketProxyPort});
+  console.log(' ');
+  console.log(' ');
+  console.log(' ');
+  console.log(`Websocket server listening on port ws://localhost:${NMSConfig.mqttWebsocketProxyPort}`);
 
-
-ws.on('connection', (connectingWS) => {
-  console.log('New client connected to Websocket');
-  connections.add(connectingWS);
-  
-  connectingWS.on('message', (data) => {
-    try {
-      data = JSON.parse(data);
-      // console.log('[LOG]=== Websocket incoming topic:', data.topic);
-      console.log('[LOG]=== Websocket incoming total data:', data);
-      // console.log('[LOG]=== Websocket incoming message data:', data.message);
-      if (data.topic === 'connectToMQTT') {
-        let ip = data.message;
-        connectToMQTT(ip);
-      } else if (data.topic === 'subscribe') {
-        subscribeToTopic(data.message);
-      } else {
-        // Send to MQTT
-        const msg = JSON.stringify(data.message);
-        console.log('[LOG]=== Proxy forwards to MQTT: ');
-        console.log('[LOG]=== Topic: ', data.topic);
-        console.log('[LOG]=== Message: ', msg);
-        mqttClient.publish(data.topic, msg, (error) => {
-          if (error) console.error(error);
-        });
+  ws.on('connection', (connectingWS) => {
+    console.log('New client connected to Websocket');
+    connections.add(connectingWS);
+    
+    connectingWS.on('message', (data) => {
+      try {
+        data = JSON.parse(data);
+        // console.log('[LOG]=== Websocket incoming topic:', data.topic);
+        console.log('[LOG]=== Websocket incoming total data:', data);
+        // console.log('[LOG]=== Websocket incoming message data:', data.message);
+        if (data.topic === 'connectToMQTT') {
+          let ip = data.message;
+          connectToMQTT(ip);
+        } else if (data.topic === 'subscribe') {
+          subscribeToTopic(data.message);
+        } else {
+          // Send to MQTT
+          const msg = JSON.stringify(data.message);
+          console.log('[LOG]=== Proxy forwards to MQTT: ');
+          console.log('[LOG]=== Topic: ', data.topic);
+          console.log('[LOG]=== Message: ', msg);
+          mqttClient.publish(data.topic, msg, (error) => {
+            if (error) console.error(error);
+          });
+        }
+        
+      } catch (error) {
+        console.log(error, 'Message recieved:', data.toString());
       }
-      
-    } catch (error) {
-      console.log(error, 'Message recieved:', data.toString());
-    }
+    });
+  
+    connectingWS.on('close', () => {
+      connections.delete(connectingWS);
+      console.log('Client has disconnected');
+    });
   });
+  
 
-  connectingWS.on('close', () => {
-    connections.delete(connectingWS);
-    console.log('Client has disconnected');
-  });
-});
+
+  // check the queue 
+  setInterval(() => {
+    if (connections.size > 0) {
+      while (messageQueue.length > 0) {
+        let data = messageQueue.pop();
+        sendToWS(data);
+      }
+    }
+  }, 1000);
+
+}
+
+
 
 function sendToWS(data) {
   if (connections.size > 0) {
@@ -110,6 +129,7 @@ function sendToWS(data) {
 let mqttClient; 
 
 
+// Called from inside Imagine
 function connectToMQTT(ip) {
 
   if (mqttClient) {
@@ -145,13 +165,8 @@ function connectToMQTT(ip) {
   
   mqttClient.on('message', function(topic, message) {
     console.log(`[LOG]================= message received from MQTT =============: ${topic.toString()} : ${message.toString()}`);
-    // console.log("[LOG]================= mqttMessageDispatch to WS ====:", topic ,' with: ',  message);
-    // Send to WS
+    // Send to WebSocket
     sendToWS({topic: topic, message: message.toString()});
-
-    // Debug
-    // debugBounceMessage({topic: topic, message: message});
-    // debugSendMessage();
   });
 }
 
